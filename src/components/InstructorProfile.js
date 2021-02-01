71069d8135b1bb8b2b4bf52053bbd70c43bde1ca
Shadi497 <b.shadi.h@gmail.com>
2021-02-01 10:42:34 +0300

    DONE

--- src/components/InstructorProfile.js
+++ src/components/InstructorProfile.js
@@ -1,11 +1,13 @@
 import { Button, ButtonsWrapper } from "./styles";
+import { Redirect, Link, useParams } from "react-router-dom";
 
-const InstructorProfile = ({ instructors, instructorSlug, goTo }) => {
+const InstructorProfile = ({ instructors }) => {
+  const { instructorSlug } = useParams();
   const instructor = instructors.find(
     (instructor) => instructor.slug === instructorSlug
   );
 
-  if (!instructor) goTo("/404");
+  if (!instructor) return <Redirect exact to="/404" />;
 
   const { name, emoji, github, description } = instructor;
 
@@ -18,11 +20,11 @@ const InstructorProfile = ({ instructors, instructorSlug, goTo }) => {
       <p>{description}</p>
 
       <ButtonsWrapper>
-        <div onClick={() => goTo("/")}>
+        <Link to={`/`}>
           <Button color="tomato" textColor="white">
             Go back home
           </Button>
-        </div>
+        </Link>
         <Button onClick={goToGitHub}>Go to GitHub</Button>
       </ButtonsWrapper>
     </>
